67ffe325b0831a03b006acc8763e9a6b86021bb7
Aman kumar <amank80459@gmail.com>
2026-03-07 12:01:54 +0530

    updated folder

--- src/components/common/Container.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react"
-
-const Container = ({ children }: PropsWithChildren) => {
-  return <div className="mx-auto w-full max-w-6xl px-6">{children}</div>
-}
-
-export default Container
